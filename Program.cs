df15cca4b7f010c5c31dc0862a8b8722b7974db7
Cesar Jose Godoy <48810223+cesarjosegodoy@users.noreply.github.com>
2025-06-07 00:32:36 -0300

    Ejercicio 3 - CalculadoraV2

--- Program.cs
+++ Program.cs
@@ -1,38 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Ejercicio 3 Calculadora 3
 
-int a;
-
-int b;
-
-a = 10;
-
-b = a;
-
-Console.WriteLine("valor de a:" + a);
-Console.WriteLine("valor de b:" + b);
-
-//Ejercicio 1
-
-Console.WriteLine("Ingrese un numero:");
-string entrada = Console.ReadLine();
-int num; 
-bool resultado = int.TryParse(entrada, out num);
-
-if (resultado && num > 0)
+Console.WriteLine("Ingrese un numero: ");
+float numero2 = float.Parse(Console.ReadLine());
+Console.WriteLine("\n\tEl valor absoluto del numero es: " + float.Abs(numero2));
+Console.WriteLine("\n\tEl cuadrado del numero es: " + float.Pow(numero2, 2));
+if (numero2 > 0)
 {
-    Console.WriteLine("Es un numero");
-    int invertido = 0;
-    while (num > 0)
-    {
-        int digito = num % 10;
-        invertido = (invertido * 10) + digito;
-        num /= 10;
-    }
-    Console.WriteLine(invertido);
-
+    Console.WriteLine("\n\tLa raiz cuadrada del numero es: " + float.Sqrt(numero2));
 }
 else
 {
-    Console.WriteLine("No es un numero");
+    Console.WriteLine("\n\t" + float.Sqrt(numero2) + " no posee raiz cuadrada");
 }
+
+if (float.IsEvenInteger(float.Sin(numero2)))
+{
+    Console.WriteLine("\n\tEl seno del numero es: " + float.Sin(numero2));
+}
+else
+{
+    Console.WriteLine("\n\t" + numero2 + " no posee seno");
+}
+
+if (float.IsEvenInteger(float.Cos(numero2)))
+{
+    Console.WriteLine("\n\tEl coseno del numero es: " + float.Cos(numero2));
+}
+else
+{
+    Console.WriteLine("\n\t" + numero2 + " no posee coseno");
+}
+Console.WriteLine("\n\tla parte entera del numero " + numero2 + " es " + (int)numero2);
+
+List<float> lista = [];
+
+Console.WriteLine("Ingrese un numero: ");
+
+lista.Add(float.Parse(Console.ReadLine()));
+
+Console.WriteLine("Ingrese otro numero: ");
+
+lista.Add(float.Parse(Console.ReadLine()));
+
+Console.WriteLine("El numero maximo es: " + lista.Max());
+
+Console.WriteLine("El numero minimo es: " + lista.Min());
